8acbea5ecddb4ffa159139b71a1df2cc4d62436e
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2024-11-30 14:49:20 +0100

    creation fichier Shell

---
+++ struct.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct noeud{
+    int cent;
+    int hvb;
+    int e_hvb;
+    int hva;
+    int e_hva;
+    int lv;
+    int e_lv;
+    int p_lv;
+}Neud;
+
+typedef struct arbre{
+    int s_cons;
+}Arbre;
+
+
+int main()
+{
+
+    return 0;
+}
