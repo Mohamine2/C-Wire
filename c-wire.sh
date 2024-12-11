c15c70198180b7faddadd11be66a3439bb84b817
alexis <alexismurugan@gmail.com>
2024-12-11 14:57:36 +0100

    ajout de help à chaque erreurs

--- c-wire.sh
+++ c-wire.sh
@@ -19,28 +19,78 @@ fi
 # Vérification des paramètres obligatoires
 if [[ $# -lt 3 ]]; then
   echo "Erreur : Les paramètres <chemin_fichier>, <type_station>, et <type_consommateur> sont obligatoires."
+  echo "Usage: $0 <chemin_fichier> <type_station> <type_consommateur> [identifiant_centrale] [-h]"
+  echo "Paramètres obligatoires :"
+  echo "  <chemin_fichier> : Indique l’endroit où se trouve le fichier d’entrée."
+  echo "  <type_station> : Type de station (valeurs possibles : hvb, hva, lv)."
+  echo "  <type_consommateur> : Type de consommateur (valeurs possibles : comp, indiv, all)."
+  echo "    ATTENTION : hvb et hva ne peuvent pas être associés à all ou indiv."
+  echo "Paramètre optionnel :"
+  echo "  [identifiant_centrale] : filtre les résultats pour une centrale spécifique. Si cette option est absente, les traitements seront effectués sur toutes les centrales du fichier"
+  echo "Options :"
+  echo "  -h : Affiche cette aide et ignore toutes les autres options."
   exit 1
 fi
 
 #Vérification des valeurs
 if [[ ! -f "$1" ]]; then
   echo "Erreur : Le fichier spécifié n'existe pas"
+  echo "Usage: $0 <chemin_fichier> <type_station> <type_consommateur> [identifiant_centrale] [-h]"
+  echo "Paramètres obligatoires :"
+  echo "  <chemin_fichier> : Indique l’endroit où se trouve le fichier d’entrée."
+  echo "  <type_station> : Type de station (valeurs possibles : hvb, hva, lv)."
+  echo "  <type_consommateur> : Type de consommateur (valeurs possibles : comp, indiv, all)."
+  echo "    ATTENTION : hvb et hva ne peuvent pas être associés à all ou indiv."
+  echo "Paramètre optionnel :"
+  echo "  [identifiant_centrale] : filtre les résultats pour une centrale spécifique. Si cette option est absente, les traitements seront effectués sur toutes les centrales du fichier"
+  echo "Options :"
+  echo "  -h : Affiche cette aide et ignore toutes les autres options."
   exit 1
 fi
 
 if [[ "$2" != "hvb" && "$2" != "hva" && "$2" != "lv" ]]; then
 	echo "Erreur : Type de station incorrect."
+  echo "Usage: $0 <chemin_fichier> <type_station> <type_consommateur> [identifiant_centrale] [-h]"
+  echo "Paramètres obligatoires :"
+  echo "  <chemin_fichier> : Indique l’endroit où se trouve le fichier d’entrée."
+  echo "  <type_station> : Type de station (valeurs possibles : hvb, hva, lv)."
+  echo "  <type_consommateur> : Type de consommateur (valeurs possibles : comp, indiv, all)."
+  echo "    ATTENTION : hvb et hva ne peuvent pas être associés à all ou indiv."
+  echo "Paramètre optionnel :"
+  echo "  [identifiant_centrale] : filtre les résultats pour une centrale spécifique. Si cette option est absente, les traitements seront effectués sur toutes les centrales du fichier"
+  echo "Options :"
+  echo "  -h : Affiche cette aide et ignore toutes les autres options."
 	exit 1
 fi
 
 if [[ "$3" != "comp" && "$3" != "indiv" && "$3" != "all" ]]; then
 	echo "Erreur : Type de consommateur incorrect."
+  echo "Usage: $0 <chemin_fichier> <type_station> <type_consommateur> [identifiant_centrale] [-h]"
+  echo "Paramètres obligatoires :"
+  echo "  <chemin_fichier> : Indique l’endroit où se trouve le fichier d’entrée."
+  echo "  <type_station> : Type de station (valeurs possibles : hvb, hva, lv)."
+  echo "  <type_consommateur> : Type de consommateur (valeurs possibles : comp, indiv, all)."
+  echo "    ATTENTION : hvb et hva ne peuvent pas être associés à all ou indiv."
+  echo "Paramètre optionnel :"
+  echo "  [identifiant_centrale] : filtre les résultats pour une centrale spécifique. Si cette option est absente, les traitements seront effectués sur toutes les centrales du fichier"
+  echo "Options :"
+  echo "  -h : Affiche cette aide et ignore toutes les autres options."
 	exit 1
 fi
 
 # Vérification des contraintes sur les types de station et de consommateur
 if [[ "$2" == "hvb" || "$2" == "hva" ]] && [[ "$3" != "comp" ]]; then
   echo "Erreur : Les types de station hvb et hva ne peuvent être associés qu'à 'comp'."
+  echo "Usage: $0 <chemin_fichier> <type_station> <type_consommateur> [identifiant_centrale] [-h]"
+  echo "Paramètres obligatoires :"
+  echo "  <chemin_fichier> : Indique l’endroit où se trouve le fichier d’entrée."
+  echo "  <type_station> : Type de station (valeurs possibles : hvb, hva, lv)."
+  echo "  <type_consommateur> : Type de consommateur (valeurs possibles : comp, indiv, all)."
+  echo "    ATTENTION : hvb et hva ne peuvent pas être associés à all ou indiv."
+  echo "Paramètre optionnel :"
+  echo "  [identifiant_centrale] : filtre les résultats pour une centrale spécifique. Si cette option est absente, les traitements seront effectués sur toutes les centrales du fichier"
+  echo "Options :"
+  echo "  -h : Affiche cette aide et ignore toutes les autres options."
   exit 1
 fi
 
