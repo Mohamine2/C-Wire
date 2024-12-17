6ef3869392ae6834c64aa5ea5abc6d70aee429d5
Mohamine2 <168640321+Mohamine2@users.noreply.github.com>
2024-12-17 14:16:14 +0100

    Creation fichier traitementAVL.h

---
+++ codeC/traitementAVL.h
@@ -0,0 +1,9 @@
+#ifndef TRAITEMENTAVL_H
+#define TRAITEMENTAVL_H
+#include "./struct.h"
+#include "./implementationAVL.h"
+
+void remplirAVL(pArbre a);
+void parcoursSomme(pArbre a, int* somme);
+
+#endif
