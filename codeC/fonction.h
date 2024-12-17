4c7df010c3302e5e3bb1de1eb058c98005c30134
IsmaNkt <ismailniakate@gmail.com>
2024-12-17 18:34:12 +0100

    Update fonction.h

--- codeC/fonction.h
+++ codeC/fonction.h
@@ -1,18 +1,6 @@
-#ifndef ENTRAINEMENT_H
-#define ENTRAINEMENT_H
+#ifndef FONCTION_H
+#define FONCTION_H
 
-pArbre CreerArbre(int id, int cap, int consommation);
-int max(int a,int b);
-int min(int a,int b);
-int min2(int a,int b,int c);
-pArbre rotationGauche(pArbre a);
-pArbre rotationDroite(pArbre a);
-pArbre DoubleRotationGauche(pArbre a);
-pArbre DoubleRotationDroite(pArbre a);
-pArbre equilibreAVL(pArbre a);
-pArbre insertionAVL(pArbre a, int id, int capacite, int consommation, int* h);
-void afficherInfixe(pArbre a);
-int recherche(pArbre a, int id, int consommation);
 void remplirAVL(pARbre a);
 parcoursSomme(pARbre a, int* somme);
 #endif
