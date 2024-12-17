0c0b699928ed912c0184b77b8f2d92fd1a551308
Mohamine2 <168640321+Mohamine2@users.noreply.github.com>
2024-12-17 14:11:26 +0100

    Creation fichier implementationAVL.h

---
+++ codeC/implementationAVL.h
@@ -0,0 +1,18 @@
+#ifndef IMPLEMENTATIONAVL_H
+#define IMPLEMENTATIONAVL_H
+#include "./struct.h"
+
+pArbre CreerArbre(int id, int cap, int consommation);
+int max(int a,int b);
+int min(int a,int b);
+int min3(int a,int b,int c);
+pArbre rotationGauche(pArbre a);
+pArbre rotationDroite(pArbre a);
+pArbre DoubleRotationGauche(pArbre a);
+pArbre DoubleRotationDroite(pArbre a);
+pArbre equilibreAVL(pArbre a);
+pArbre insertionAVL(pArbre a, int id, int capacite, int consommation, int* h);
+void afficherInfixe(pArbre a);
+int recherche(pArbre a, int id, int consommation);
+
+#endif
