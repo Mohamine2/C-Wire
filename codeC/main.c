c702b487e48c94e5d804733f6647b79eef3fba9b
alexis <alexismurugan@gmail.com>
2024-12-19 00:39:16 +0100

    modifs sur main

--- codeC/main.c
+++ codeC/main.c
@@ -11,13 +11,17 @@
 
 int main(int argc, char *argv[]){
 
+    if (argc < 2) { // Vérifier qu'il y a au moins un argument passé
+        printf("Erreur : Veuillez fournir un argument.\n");
+        return 1;
+    }
     int chiffre = atoi(argv[1]);
     char ligne[TAILLE_MAX_LIGNE];
     
      if(chiffre == 6){
         //code pour lv min max
         //faudrait le faire avec une liste chainée pour insérer les noeuds 
-        int station, h;
+        int station;
         long capacite, conso;
         fgets(ligne, sizeof(ligne), stdin); //ignorer la première ligne
 
